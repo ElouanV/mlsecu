e05abb8026cb99126f9103cc6e39f90120249960
Elouan Vincent <elouan.vincent@epita.fr>
2023-11-16 11:54:13 +0100

    add: univariate and bivariate plots in lib

--- src/data_exploration_utils.py
+++ src/data_exploration_utils.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 def get_column_names(dataframe):
     """Get the name of columns in the dataframe
      
@@ -17,6 +21,7 @@ def get_nb_of_dimensions(dataframe):
         return None
     return dataframe.shape[1]
 
+
 def get_nb_of_rows(dataframe):
     """Get the number of rows
      
@@ -26,6 +31,7 @@ def get_nb_of_rows(dataframe):
         return None
     return dataframe.shape[0]
 
+
 def get_number_column_names(dataframe):
     """Get the name of numeric columns
      
@@ -34,6 +40,7 @@ def get_number_column_names(dataframe):
     if dataframe is None:
         return None
     return dataframe.select_dtypes(include=["number"]).columns.values.tolist()
+
 
 def get_object_column_names(dataframe):
     """Get the name of object columns
@@ -54,3 +61,80 @@ def get_unique_values(dataframe, column_name):
     if dataframe is None:
         return None
     return dataframe[column_name].unique()
+
+
+def plot_univariate_histogram(dataframe, column_name):
+    """Plot the histogram of a given column
+
+    :param dataframe: input dataframe
+    :param column_name: target column label
+    :return: None"""
+    if dataframe is None:
+        return None
+
+    plt.hist(dataframe[column_name])
+
+
+def plot_univariate_boxplot(dataframe, column_name):
+    """Plot the boxplot of a given column
+
+    :param dataframe: input dataframe
+    :param column_name: target column label
+    :return: None"""
+    if dataframe is None:
+        return None
+
+    plt.boxplot(dataframe[column_name])
+
+
+def plot_all_univariate_histogram(dataframe):
+    """Plot the histogram of all columns
+
+    :param dataframe: input dataframe
+    :return: None"""
+    if dataframe is None:
+        return None
+
+    for column_name in get_number_column_names(dataframe):
+        plot_univariate_histogram(dataframe, column_name)
+        plt.show()
+
+
+def plot_all_univariate_boxplot(dataframe):
+    """
+    Plot the boxplot of all columns
+    :param dataframe:
+    :return:
+    """
+    if dataframe is None:
+        return None
+    for column_name in get_number_column_names(dataframe):
+        plot_univariate_boxplot(dataframe, column_name)
+        plt.show()
+
+
+def plot_bivariate_scatterplot(dataframe, column_name1, column_name2):
+    """
+    Plot the scatterplot of two given columns
+    :param dataframe: input dataframe
+    :param column_name1: target column label
+    :param column_name2: target column label
+    :return: None"""
+    if dataframe is None:
+        return None
+    plt.scatter(dataframe[column_name1], dataframe[column_name2])
+
+
+def plot_all_bivariate_scatterplot(dataframe):
+    """
+    Plot the scatterplot of all columns
+    :param dataframe: input dataframe
+    :return: None"""
+    if dataframe is None:
+        return None
+    for column_name1 in get_number_column_names(dataframe):
+        for column_name2 in get_number_column_names(dataframe):
+            plot_bivariate_scatterplot(dataframe, column_name1, column_name2)
+            plt.show()
+
+
